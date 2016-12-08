3bb7439b9141c2d34faf986178b60b6891444895
Jonathan Zungre <jzungre@gmail.com>
2016-12-08 16:52:34 -0500

    added duck and almost done with image upload

--- Client/inputController.js
+++ Client/inputController.js
@@ -1,20 +1,29 @@
-angular.module('ChooseForMe',[]) // naming the module
+angular.module('ChooseForMe',['ngFileUpload']) // naming the module
 	.controller('inputController', function($scope, $http){ // defining the controller, inject $scope and $http
 		$scope.entry;
-		$scope.username;
+		$scope.username = 'Anonymous';
 		$scope.password;
 
-
+		$scope.newData = []
+		$scope.pictures = [];
 		$scope.object = {};
 		$scope.data = [];
 		$scope.temp = []
 			// declare the $scope.entry property
 
+		$scope.submit = function(file){
+			console.log('File, yo!', file);
+
+		}
+
 		$scope.save = function(){ // create a function that will send entered art to server
 			console.log("$scope.save called with: ", $scope.entry);
+			var newEntry = $scope.entry;
+			$scope.newData.push(newEntry)
+			console.log($scope.newData);
 			$scope.object.data  = $scope.username + ': ' + $scope.entry;
 			var data = JSON.stringify($scope.object);
-			console.log("STRingafy me, baby!", data);
+			console.log("STRingafied data", data);
 			$http({
 				'method': 'POST',
 				'url': '/list',
@@ -38,9 +47,49 @@ angular.module('ChooseForMe',[]) // naming the module
 				});
 		  	}, function errorCallback(response) {
 		  		console.log('uh oh, we got an error: ', response);
-		    // called asynchronously if an error occurs
-		    // or server returns response with an error status.
 		  	});
+		}
+		$scope.add = function(){
+  			console.log("in add")
+  			var f = document.getElementById('file').files[0];
+  			console.log("in add with pre-json f", f);
+      		r = new FileReader();
+      		$scope.object.data  = f;
+			var data = JSON.stringify($scope.object);
+      		console.log('post json f: ', f);
+      		$http({
+				'method': 'POST',
+				'url': '/upload',
+				'Content-Type': 'application/json',
+				'data': data
+			})
+			.then(function successCallback(response) {
+			   console.log('imageSuccess: ', response);
+			   	
+		  	}, function errorCallback(response) {
+		  		console.log('uh oh, we go an image error: ', response);
+		  	});
+  	// 		r.onloadend = function(e){
+   //  		var data = e.target.result;
+   //  		//console.log('dat data: ', data);
+   //  //send your binary data via $http or $resource or do anything else with it
+   //  		$http({
+			// 	'method': 'POST',
+			// 	'url': '/upload',
+			// 	'Content-Type': 'text/plain',
+			// 	'data': data
+			// })
+			// .then(function successCallback(response) {
+			//    console.log('imageSuccess: ', response);
+			   	
+		 //  	}, function errorCallback(response) {
+		 //  		console.log('uh oh, we go an image error: ', response);
+		 //  	});
+
+
+
+  	// 		}
+  		//	r.readAsBinaryString(f);
 		}
 
 	$scope.list = function(){
--- Server/server.js
+++ Server/server.js
@@ -85,68 +85,6 @@ server.get('/list', function(req, res){
 
 })
 
-server.post('/signup', function (req, res) {   
-	  console.log('req.body:', req.body);
-  // save username and password in variables from request                   //1. grab data from req
-  var username = req.body.username;                                             
-  var password = req.body.password
-  // ---
-  // TODO:
-  //   1. Make sure username is not taken
-      if(User.findByUsername(username) === null){                           //2. make sure username is not taken
-      //2. Create user
-  console.log('POSTusername:', username, 'POSTpassword:', password);
-  User.create(username, password)                                           //3. create a user
-  res.sendStatus(201) // send an asset created sendStatus                   //4. send 'asset created' status
-  } else {                                                                  
-    //console.log('res.body.reason', res.body.reason)
-    res.status(400).send({reason: "username_is_taken"});                    //5. if the username is taken send '400'
-    // res.body.reason = 'username_is_taken'
-    // res.status(400);
-    // res.send(res.body.reason)
-    //res.sendStatus(400);
-  }
-  //   3. Send back 201
-  // ---
-
-
-
-
-})
-
-server.post('/signin', function(req, res){
-
-    var username = req.body.username;                                          //1. save username and password in variables from request
-    var password = req.body.password;
-  // ---
-  // TODO:
-  //   1. Attempt to find user by username
-    var user = User.findByUsername(username);                                  //2. find user by username
-    // var verdict = User.matchesPassword(user, password);
-    console.log('username: ', username, 'password: ', password, 'user: ', user);
-    
-  //   2. Make sure passwords match
-  if(user === null){                                                           //3. send back error if user isn't found
-    res.status(400).send({reason: 'no_such_username'})
-  }else{
-
-     if(User.matchesPassword(user, password) === true){                        //4. see if entered password, when hashed, matches the stored hashed password
-      //   3. Create a new session
-          var session = Session.create(user.id);                               //5. if they do, create a session, send back an asset created status and the session id
-          console.log('Session Sweeeeezy!', session);
-      //   4. Send back new session's id (201)
-          res.status(201).send({sessionId: session.id})                       
-     }else{
-        res.status(401).send({reason: 'incorrect_password'})                   //6. send back a 401, if the passwords don't match
-     }
-  }
-  // ---
-
-
-
-
-})
-
 //Orient ------ server up static files
 server.use(express.static(__dirname + "/../Client"))
 
